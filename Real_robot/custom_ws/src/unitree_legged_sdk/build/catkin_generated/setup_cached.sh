acd30d9a792a1fdc6c8f202c4cb96c42e4b6df89
BercuCristiDaniel <cristi_daniel.bercu@stud.acs.upb.ro>
2025-07-18 16:16:22 +0200

    update

---
+++ Real_robot/custom_ws/src/unitree_legged_sdk/build/catkin_generated/setup_cached.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH='/home/cristi/Desktop/custom_ws/src/unitree_legged_sdk/build/devel:/usr/local:/opt/ros/noetic'
+export LD_LIBRARY_PATH='/usr/local/lib:/opt/ros/noetic/lib'
+export PATH='/usr/local/bin:/opt/ros/noetic/bin:/home/cristi/.local/bin:/usr/local/sbin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin'
+export ROSLISP_PACKAGE_DIRECTORIES='/home/cristi/Desktop/custom_ws/src/unitree_legged_sdk/build/devel/share/common-lisp'
+export ROS_PACKAGE_PATH="/home/cristi/Desktop/custom_ws/src/unitree_legged_sdk:/usr/local/share:$ROS_PACKAGE_PATH"
